5f5e73fb5093991efbc4c73297fa739c7abcc7dd
Gaurab Ghosh <gaurab.ghosh90@gmail.com>
2014-09-13 22:20:52 -0500

    Adding pearson module

--- ClusteringBlog/cluster.py
+++ ClusteringBlog/cluster.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def readFile(filename):
 	lines = [line for line in file(filename)]
 
@@ -11,5 +13,22 @@ def readFile(filename):
 		data.append([float(x) for x in p[1:]])
 	return colnames, rownames, data
 
+def pearson(x, y):
+	print 2**2
+	meanx = sum(x)/len(x)
+	meany = sum(y)/len(y)
+	sumx = 0.0
+	sumy = 0.0
+	sumxy = 0.0
+	for i in x:
+		sumx += (i - meanx)**2
+	for j in y:
+		sumy += (j - meany)**2
+	for index, val in enumerate(x):
+		sumxy += (x[index] - meanx)*(y[index] - meany)
+	r = sumxy/(sqrt(sumx)*sqrt(sumy))
 
-readFile("blogdata.txt")
+	return r
+
+#readFile("blogdata.txt")
+print pearson([1,2,3], [6,4,1])
